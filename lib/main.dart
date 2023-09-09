03ef882afa04d81a8e658f2f565d1416a41ae511
checho0417 <saavedrarojass41@gmail.com>
2023-09-08 20:48:30 -0500

    hj2

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voice Assistant',
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
